9c5f26da5111b11b9826662e300d8ee474359dcb
s-pace <sylvain.pace@algolia.com>
2019-09-11 10:21:41 +0200

    ci: use correct path

--- .circleci/json_check.sh
+++ .circleci/json_check.sh
@@ -1,6 +1,21 @@
-#!/bin/bash
-FILES=/configs/*
+#!/usr/bin/env bash
+
+# Exit script if you try to use an uninitialized variable.
+set -o nounset
+
+# Exit script if a statement returns a non-true return value.
+set -o errexit
+
+# Use the error status of the first failure, rather than that of the last item in a pipeline.
+set -o pipefail
+
+FILES=./configs/*
 for f in $FILES
 do
-  jq -e $f
+  echo $f
+
+  if [ ! jq -e -f $f ] then
+    echo 'Failed to parse configuration $f, or got false/null'
+    exit 1
+  fi
 done
