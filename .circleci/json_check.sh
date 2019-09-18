b7e60c1eaf95f47d0ac1d18dfbb83b7b7592ade7
Sylvain Pace <sylvain.pace@algolia.com>
2019-09-18 18:16:12 +0200

    ci: check every config are a well formatted JSON file (#1028)
    * ci: use correct path
    
    * feat(v0): check if JSON is well formated

--- .circleci/json_check.sh
+++ .circleci/json_check.sh
@@ -1,6 +1,16 @@
-#!/bin/bash
-FILES=/configs/*
+#!/usr/bin/env bash
+
+# Exit script if you try to use an uninitialized variable.
+set -o nounset
+
+# Exit script if a statement returns a non-true return value.
+set -o errexit
+
+# Use the error status of the first failure, rather than that of the last item in a pipeline.
+set -o pipefail
+
+FILES=./configs/*
 for f in $FILES
 do
-  jq -e $f
+  jq -e '.nb_hits' 1>/dev/null < "$f" || { echo "Issue with ${f} is not well formated and/or missing nb_hits key"; exit 1;}
 done
