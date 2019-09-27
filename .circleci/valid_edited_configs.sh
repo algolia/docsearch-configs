91de749c8ab694237aa4618ec8a4ed8f822458f3
Sylvain Pace <sylvain.pace@algolia.com>
2019-09-27 17:02:08 +0200

    feat(CI): valid edited configs against validator (#1045)
    * feat(CI): valid edited configs against validator
    
    * feat(ci): exit when validator provides a message

---
+++ .circleci/valid_edited_configs.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Exit script if you try to use an uninitialized variable.
+set -o nounset
+
+# Exit script if a statement returns a non-true return value.
+set -o errexit
+
+# Use the error status of the first failure, rather than that of the last item in a pipeline.
+set -o pipefail
+
+FILES=$(git diff master --name-only ./configs)
+for f in $FILES
+do
+    # Valid JSON and look for nb_hits
+    jq -e '.nb_hits' 1>/dev/null < "$f" || { echo "Issue with ${f} is not well formated and/or missing nb_hits key"; exit 1;}
+    # Valid config according to JSON schema
+    jq -e 'tojson | {config:.}' < "$f" | curl -X POST -s -d @- -H "Content-Type: application/json" https://docsearch-hub.herokuapp.com/api/docsearch/validator/ 2>&1 | jq -e '.[0].message' && { echo "Config ${f} is not compliant with JSON schema due to ^"; exit 1;}
+done
