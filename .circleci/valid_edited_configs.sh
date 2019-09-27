a4f869e1d756f9b205e525b21999044abd61cd2e
s-pace <sylvain.pace@algolia.com>
2019-09-27 16:14:04 +0200

    feat(CI): valid edited configs against validator

--- .circleci/valid_edited_configs.sh
+++ .circleci/valid_edited_configs.sh
@@ -9,8 +9,11 @@ set -o errexit
 # Use the error status of the first failure, rather than that of the last item in a pipeline.
 set -o pipefail
 
-FILES=./configs/*
+FILES=$(git diff master --name-only ./configs)
 for f in $FILES
 do
+  # Valid JSON and look for nb_hits
   jq -e '.nb_hits' 1>/dev/null < "$f" || { echo "Issue with ${f} is not well formated and/or missing nb_hits key"; exit 1;}
+  # Valid config according to JSON schema
+  jq -e 'tojson | {config:.}' < "$f" | curl -X POST -d @- -H "Content-Type: application/json" https://docsearch-hub.herokuapp.com/api/docsearch/validator/
 done
