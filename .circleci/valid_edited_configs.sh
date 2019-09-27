31fe26a9b66a6ba687d63fa9cfe1dcc771887238
s-pace <sylvain.pace@algolia.com>
2019-09-27 17:18:45 +0200

    Revert "debugging"
    This reverts commit c6c1d5902ed7381791f03dc812fb2c5e25a7eb31.

--- .circleci/valid_edited_configs.sh
+++ .circleci/valid_edited_configs.sh
@@ -9,9 +9,7 @@ set -o errexit
 # Use the error status of the first failure, rather than that of the last item in a pipeline.
 set -o pipefail
 
-git diff origin/master --name-only ./configs
-
-FILES=$(git diff origin/master --name-only ./configs)
+FILES=$(git diff master --name-only ./configs)
 for f in $FILES
 do
     # Valid JSON and look for nb_hits
