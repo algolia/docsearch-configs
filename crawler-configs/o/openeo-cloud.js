6b48ea77d0989064ad9b40c40fbe587915260c6e
Clément Vannicatte <vannicattec@gmail.com>
2021-11-05 09:24:50 +0100

    feat(crawler): update crawler configs

--- crawler-configs/o/openeo-cloud.js
+++ crawler-configs/o/openeo-cloud.js
@@ -2,34 +2,217 @@ new Crawler({
   appId: "",
   apiKey: "",
   rateLimit: 8,
-  startUrls: ["https://docs.openeo.cloud/"],
-  renderJavaScript: false,
+  startUrls: [
+    "https://open-eo.github.io/openeo-js-client/latest/",
+    "https://open-eo.github.io/",
+    "https://open-eo.github.io/openeo-r-client/",
+    "https://open-eo.github.io/openeo-python-client/",
+    "https://docs.openeo.cloud/file-formats/",
+    "https://docs.openeo.cloud/",
+    "https://docs.openeo.cloud/data-collections/",
+    "https://docs.openeo.cloud/processes/",
+    "https://docs.openeo.cloud/api/",
+  ],
+  renderJavaScript: true,
   sitemaps: [],
   exclusionPatterns: [],
   ignoreCanonicalTo: false,
-  discoveryPatterns: ["https://docs.openeo.cloud/**"],
+  discoveryPatterns: [
+    "https://open-eo.github.io/**",
+    "https://docs.openeo.cloud/**",
+  ],
   schedule: "at 20:30 on Thursday",
   actions: [
+    {
+      indexName: "openeo-cloud",
+      pathsToMatch: ["https://open-eo.github.io/openeo-js-client/latest/**"],
+      recordExtractor: ({ $, helpers }) => {
+        // Removing DOM elements we don't want to crawl
+        const toRemove = ".table-of-contents, .toc";
+        $(toRemove).remove();
+
+        return helpers.docsearch({
+          recordProps: {
+            lvl1: "#main h1",
+            content: "#main p, #main li, #main td",
+            lvl0: {
+              selectors: "#title",
+              defaultValue: "JavaScript Client",
+            },
+            lvl2: "#main h2",
+            lvl3: "#main h3",
+            lvl4: "#main h4",
+            lvl5: "#main h5",
+          },
+          indexHeadings: true,
+        });
+      },
+    },
+    {
+      indexName: "openeo-cloud",
+      pathsToMatch: ["https://open-eo.github.io/openeo-r-client/**"],
+      recordExtractor: ({ $, helpers }) => {
+        // Removing DOM elements we don't want to crawl
+        const toRemove = ".table-of-contents, .toc";
+        $(toRemove).remove();
+
+        return helpers.docsearch({
+          recordProps: {
+            lvl1: ".contents h1",
+            content: ".contents p, .contents li",
+            lvl0: {
+              selectors: "#title",
+              defaultValue: "R Client",
+            },
+            lvl2: ".contents h2",
+            lvl3: ".contents h3",
+            lvl4: ".contents h4",
+            lvl5: ".contents h5",
+          },
+          indexHeadings: true,
+        });
+      },
+    },
+    {
+      indexName: "openeo-cloud",
+      pathsToMatch: ["https://open-eo.github.io/openeo-python-client/**"],
+      recordExtractor: ({ $, helpers }) => {
+        // Removing DOM elements we don't want to crawl
+        const toRemove = ".table-of-contents, .toc";
+        $(toRemove).remove();
+
+        return helpers.docsearch({
+          recordProps: {
+            lvl1: ".body h1",
+            content: ".body p, .body li",
+            lvl0: {
+              selectors: "#title",
+              defaultValue: "Python Client",
+            },
+            lvl2: ".body h2",
+            lvl3: ".body h3",
+            lvl4: ".body h4",
+            lvl5: ".body h5",
+          },
+          indexHeadings: true,
+        });
+      },
+    },
+    {
+      indexName: "openeo-cloud",
+      pathsToMatch: ["https://docs.openeo.cloud/file-formats/**"],
+      recordExtractor: ({ $, helpers }) => {
+        // Removing DOM elements we don't want to crawl
+        const toRemove = ".table-of-contents, .toc";
+        $(toRemove).remove();
+
+        return helpers.docsearch({
+          recordProps: {
+            lvl1: ".file-formats .list li .summary strong",
+            content: ".file-formats .list li .summary small",
+            lvl0: {
+              selectors: ".file-formats h2",
+              defaultValue: "File Formats",
+            },
+            pageRank: "1",
+          },
+          indexHeadings: true,
+        });
+      },
+    },
+    {
+      indexName: "openeo-cloud",
+      pathsToMatch: ["https://docs.openeo.cloud/data-collections/**"],
+      recordExtractor: ({ $, helpers }) => {
+        // Removing DOM elements we don't want to crawl
+        const toRemove = ".table-of-contents, .toc";
+        $(toRemove).remove();
+
+        return helpers.docsearch({
+          recordProps: {
+            lvl1: ".collections .list li .summary strong",
+            content:
+              ".collections .list li .summary small, .collections .list li .summary .badge",
+            lvl0: {
+              selectors: ".collections h2",
+              defaultValue: "Collections",
+            },
+            pageRank: "2",
+          },
+          indexHeadings: true,
+        });
+      },
+    },
+    {
+      indexName: "openeo-cloud",
+      pathsToMatch: ["https://docs.openeo.cloud/processes/**"],
+      recordExtractor: ({ $, helpers }) => {
+        // Removing DOM elements we don't want to crawl
+        const toRemove = ".table-of-contents, .toc";
+        $(toRemove).remove();
+
+        return helpers.docsearch({
+          recordProps: {
+            lvl1: ".process h2",
+            content: ".process p, .process summary",
+            lvl0: {
+              selectors: ".process h1",
+              defaultValue: "Processes",
+            },
+            lvl2: ".process h4",
+            pageRank: "2",
+          },
+          indexHeadings: true,
+        });
+      },
+    },
+    {
+      indexName: "openeo-cloud",
+      pathsToMatch: ["https://docs.openeo.cloud/api/**"],
+      recordExtractor: ({ $, helpers }) => {
+        // Removing DOM elements we don't want to crawl
+        const toRemove = ".table-of-contents, .toc";
+        $(toRemove).remove();
+
+        return helpers.docsearch({
+          recordProps: {
+            lvl1: ".api-content h1",
+            content: ".api-content p, .api-content li, .api-content td",
+            lvl0: {
+              selectors: "#title",
+              defaultValue: "API",
+            },
+            lvl2: ".api-content h2",
+            lvl3: ".api-content h3",
+            lvl4: ".api-content h4",
+            lvl5: ".api-content h5",
+          },
+          indexHeadings: true,
+        });
+      },
+    },
     {
       indexName: "openeo-cloud",
       pathsToMatch: ["https://docs.openeo.cloud**/**"],
       recordExtractor: ({ $, helpers }) => {
         // Removing DOM elements we don't want to crawl
-        const toRemove = ".table-of-contents";
+        const toRemove = ".table-of-contents, .toc";
         $(toRemove).remove();
 
         return helpers.docsearch({
           recordProps: {
-            lvl1: ".theme-default-content h1",
-            content: ".theme-default-content p, .theme-default-content li",
+            lvl1: ".theme-default-content h2",
+            content:
+              ".theme-default-content p, .theme-default-content li, .theme-default-content td",
             lvl0: {
-              selectors: "p.sidebar-heading.open",
-              defaultValue: "Documentation",
+              selectors: ".theme-default-content h1",
+              defaultValue: "openEO Platform",
             },
-            lvl2: ".theme-default-content h2",
-            lvl3: ".theme-default-content h3",
-            lvl4: ".theme-default-content h4",
-            lvl5: ".theme-default-content h5",
+            lvl2: ".theme-default-content h3",
+            lvl3: ".theme-default-content h4",
+            lvl4: ".theme-default-content h5",
+            lvl5: ".theme-default-content h6",
+            pageRank: "1",
           },
           indexHeadings: true,
         });
@@ -100,6 +283,12 @@ new Crawler({
       advancedSyntax: true,
       attributeCriteriaComputedByMinProximity: true,
       removeWordsIfNoResults: "allOptional",
+      separatorsToIndex: "_",
+      synonyms: [
+        ["cube", "cubes", "datacube", "datacubes"],
+        ["js", "javascript"],
+        ["gtiff", "geotiff"],
+      ],
     },
   },
 });
